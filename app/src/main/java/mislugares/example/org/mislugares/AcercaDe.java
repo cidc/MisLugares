6d29604488440f580d4360bea6e3c1301a921a23
cesar solano <cesarsolau@hotmail.com>
2015-04-28 15:02:40 -0500

    adicion del action bar

---
+++ app/src/main/java/mislugares/example/org/mislugares/AcercaDe.java
@@ -0,0 +1,14 @@
+package mislugares.example.org.mislugares;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrador on 27/04/2015.
+ */
+public class AcercaDe extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+    }
+}
--- app/src/main/java/mislugares/example/org/mislugares/activity_main.java
+++ app/src/main/java/mislugares/example/org/mislugares/activity_main.java
@@ -1,15 +1,20 @@
 package mislugares.example.org.mislugares;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class activity_main extends ActionBarActivity {
 
+    private Button bAcercaDe;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
@@ -22,18 +27,23 @@ public class activity_main extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            if (id == R.id.action_settings) {
+                return true;
+            }
+            if (id == R.id.acercaDe) {
+                lanzarAcercaDe(null);
+                return true;
+            }
+            return true; /** true -> consumimos el item, no se propaga*/
         }
+    public void lanzarAcercaDe(View view){
+        Intent i = new Intent(this, AcercaDe.class);
+        startActivity(i);
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void salir(View view){
+        finish();
     }
 }
